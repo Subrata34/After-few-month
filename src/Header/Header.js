fb76c07705b59be54fff88ae29d97f77a8eda6be
Subrata34 <subratamodak3491@gmail.com>
2023-06-29 12:23:53 +0600

    files

---
+++ src/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>This is header files </div>
+  )
+}
+
+export default Header
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -6,6 +6,7 @@ function Login() {
         <h1>pleae Login</h1>
         <div>
             <h1>this is login files </h1>
+            <input type="submit" />
         </div>
          </div>
   )
